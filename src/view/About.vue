11654915696b420e018fc233429e9b41e17df9bf
lok660 <lokk7991@gmail.com>
2021-04-26 00:11:18 +0800

    feat:template finsh

---
+++ src/view/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    This is an about page
+    <ul>
+      <li>
+        <router-link to="/about/a">to a</router-link>
+      </li>
+      <li>
+        <router-link to="/about/b">to b</router-link>
+      </li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/view/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    This is a home page
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {}
+};
+</script>
